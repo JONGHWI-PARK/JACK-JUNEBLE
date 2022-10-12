de11f7ae3cf881f07f84ff4013c92656601694f2
PARK-MYUNG-JUN <juneblack@naver.com>
2022-10-13 00:03:33 +0900

    <Test>
    1. test kor, eng working code to add

--- project_01/src/eng_memory_book/Test.java
+++ project_01/src/eng_memory_book/Test.java
@@ -1,9 +1,12 @@
 package eng_memory_book;
 
 import java.util.Scanner; 
+import java.util.ArrayList;
 
 public class Test {
 	Scanner sc = new Scanner(System.in);
+	int correct_count;
+	int wrong_count;
 	
 	public void console()
 	{
@@ -49,11 +52,69 @@ public class Test {
 	
 	public void correct_korean(int day)
 	{
+		correct_count = wrong_count = 0;
+		
 		System.out.println("Korean, day : " + day);
+		
+		ArrayList<WordData> wd = Get_Day_Word(day);
+		
+		// test position
+		for(int i = 0; i < wd.size(); i++)
+		{
+			System.out.print("kor : " + wd.kor + " / eng : ");
+			
+			String input_str = sc.nextLine();
+			if(input_str == wd.eng)
+			{
+				correct_count++;
+			}
+			else
+			{
+				wrong_count++;
+			}
+		}
+		
+		// score position
+		System.out.print("your score is " + (double)correct_count / (correct_count + wrong_count) + "!!!\n\n");
+		
+		// feedback position
+		for(int i = 0; i < wd.size(); i++)
+		{
+			System.out.println("kor : " + wd.kor + " / eng : " + wd.eng);
+		}
 	}
 	
 	public void correct_english(int day)
 	{
 		System.out.println("English, day : " + day);
+		
+		correct_count = wrong_count = 0;
+		
+		ArrayList<WordData> wd = Get_Day_Word(day);
+		
+		// test position
+		for(int i = 0; i < wd.size(); i++)
+		{
+			System.out.print("eng : " + wd.eng + " / kor : ");
+			
+			String input_str = sc.nextLine();
+			if(input_str == wd.kor)
+			{
+				correct_count++;
+			}
+			else
+			{
+				wrong_count++;
+			}
+		}
+		
+		// score position
+		System.out.print("your score is " + (double)correct_count / (correct_count + wrong_count) + "!!!\n\n");
+		
+		// feedback position
+		for(int i = 0; i < wd.size(); i++)
+		{
+			System.out.println("kor : " + wd.eng + " / eng : " + wd.);
+		}
 	}
 }
