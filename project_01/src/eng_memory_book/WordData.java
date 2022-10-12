c94fdaaa0641fe1ed82f309594bfc1116cd0544b
jonghwi_park <whitejack.paper28@gmail.com>
2022-10-12 19:13:28 +0900

    add <WordData> : day01, day02 method

---
+++ project_01/src/eng_memory_book/WordData.java
@@ -0,0 +1,21 @@
+package eng_memory_book;
+
+import java.lang.reflect.Array;
+
+public class WordData {
+	
+	public void day01() {
+		
+		String[] eng = {"fatigue", "checkup", "symptom", "physician", "diagnosis", "prescribe", "recovery", "comprehensive", "participate", "examination"};  
+		String[] kor = {"피로", "건강 진단", "증상", "내과 의사", "진단", "(약을)처방하다", "회복, 쾌유", "종합적인, 포괄적인", "참여하다, 참가하다", "진찰, 검사"};
+		
+	}
+	
+	public void day02() {
+		
+		String[] eng = {"eliminate", "dietary", "transmit", "periodically", "coverage", "exposure", "pharmaceutical", "premium", "relive", "conscious"};  
+		String[] kor = {"제거하다", "식이 요법의, 규정식의", "전염시키다", "주기적으로", "보상 범위; 보도, 취재범위", "노출", "제약의, 약학의", "보험료", "완화시키다", "알고 있는, 자각하고 있는"};
+	
+	}
+	
+}
