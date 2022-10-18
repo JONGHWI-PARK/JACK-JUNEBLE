f4cc6940419985264d3c709baf5d8e98498d52f2
jonghwi_park <whitejack.paper28@gmail.com>
2022-10-18 14:23:06 +0900

    <Train Class>
    1. 입력한 day를 받아야하므로 train_word에 넣은 select를 select_day로
       변경
    2. String으로 변경한 데이터형 int로 다시 변경
    3. day01, day02 해당 단어 암기 코드 새로이 작성
    
    <Next Plan>
    
    1. connect DB

--- project_01/src/eng_memory_book/Train.java
+++ project_01/src/eng_memory_book/Train.java
@@ -1,39 +1,90 @@
 package eng_memory_book;
 
+import java.util.ArrayList;
 import java.util.Scanner; 
 
 public class Train {
 	Scanner sc = new Scanner(System.in);
 	
+	WordData dummy_wd = new WordData();
 	
 	public void console()
 	{
 		while(true)
 		{
-			System.out.println("1. Select days (1~2)");
+			System.out.println("1. Select days (1 ~ 2)");
 			System.out.println("2. Back");
 			System.out.print("Input Number : ");
 			
-			String select = sc.nextLine();
+			int select_menu = sc.nextInt();
 			
-			if(select.equals("3"))
+			if(select_menu == 2)
 			{
-				System.out.println("3. Back");
+				//System.out.println("3. Back");
 				break;
 			}
-
-			train_word(select);
+			
+			System.out.println("Select Day : ");
+			int select_day = sc.nextInt();
+			
+			train_word(select_day);
 		}
 	}
 	
-	public void train_word(String day)
-	{
-//		try {
-//			Thread.sleep(300); 
-//		} catch (InterruptedException e) {
-//		}
+	public void train_word(int day)
+	{ 
+		ArrayList<WordData> wd = dummy_wd.Get_Day_Word(day);
 		
-		
-		
+		for(int k = 0; k < 3; k++) {
+			
+			if(day == 1) {	// day01 단어
+				
+				System.out.println("start!!");
+				try {
+					Thread.sleep(1000); 
+				} catch (InterruptedException e) {
+				}
+				
+				for(int i = 0; i < wd.size(); i++) {
+					System.out.println(wd.get(i).eng + " " + wd.get(i).kor);
+					
+					try {
+						Thread.sleep(3000); 
+					} catch (InterruptedException e) {
+					}
+				}
+				
+				System.out.println("-------------------------------");
+				try {
+					Thread.sleep(500); 
+				} catch (InterruptedException e) {
+				}
+				
+			} else if(day == 2) {	// day02 단어
+				
+				System.out.println("start!!");
+				try {
+					Thread.sleep(1000); 
+				} catch (InterruptedException e) {
+				}
+				
+				for(int i = 0; i < wd.size(); i++) {
+					
+					System.out.println(wd.get(i).eng + " " + wd.get(i).kor);
+					
+					try {
+						Thread.sleep(3000); 
+					} catch (InterruptedException e) {
+					}
+				}
+				
+				System.out.println("-------------------------------");
+				try {
+					Thread.sleep(500); 
+				} catch (InterruptedException e) {
+				}
+				
+			}
+		}
 	}
 }
